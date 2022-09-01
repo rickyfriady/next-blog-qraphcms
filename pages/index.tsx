991dc62586ffb37bcf23a67f08abae8df1094a25
AllukyyDev <73653891+rickyfriady@users.noreply.github.com>
2022-09-01 15:32:15 +0700

    create queryy and connection on graphqlcms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import { PostCard, PostWidget, Categories } from "../components";
+import { getPosts } from "../services";
 
-const Posts = [
-  { title: "React Testing", excerpt: "Learn React testing" },
-  { title: "React With Tailwind", excerpt: "Learn React With Tailwind" },
-];
-
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }: any) => {
   return (
     <div className="container mx-auto px-10 mb-8 ">
       <Head>
@@ -17,7 +12,7 @@ const Home: NextPage = () => {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-          {Posts.map((post) => (
+          {posts.map((post: any) => (
             <PostCard post={post} key={post.title} />
           ))}
         </div>
@@ -31,5 +26,11 @@ const Home: NextPage = () => {
     </div>
   );
 };
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
 
 export default Home;
