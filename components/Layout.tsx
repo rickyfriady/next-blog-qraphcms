e1c2eb5b93960e8d2393ae21259a5dac78514223
AllukyyDev <73653891+rickyfriady@users.noreply.github.com>
2022-09-01 15:01:48 +0700

    fix navbar components and make layout components

---
+++ components/Layout.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./";
+const Layout = ({ children }: any) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Posts = [
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-400">
+    <div className="container mx-auto px-10 mb-8 ">
       <Head>
         <title>Blog CMS App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -18,9 +18,7 @@ const Home: NextPage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           {Posts.map((post) => (
-            <div>
-              <PostCard post={post} key={post.title} />
-            </div>
+            <PostCard post={post} key={post.title} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
