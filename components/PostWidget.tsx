0e74694e94036f49a7cc2e0a7b26263208bc2bf4
AllukyyDev <73653891+rickyfriady@users.noreply.github.com>
2022-09-01 22:18:05 +0700

    Update PostWidget Component ad panel related post and recent post make useState and useEffect for fetch data from grqphql make query for schema Graphql getRecentsPost and getSimilarPosts

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,5 +1,54 @@
-const PostWidget = () => {
-  return <div>PostWidget</div>;
+import moment from "moment";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+import { getRecentPosts, getSimilarPosts } from "../services";
+
+const PostWidget = ({ categories, slug }) => {
+    const [relatedPosts, setRelatedPosts] = useState([]);
+
+    useEffect(() => {
+        if (slug) {
+            getSimilarPosts(categories, slug).then((result) =>
+                setRelatedPosts(result)
+            );
+        } else {
+            getRecentPosts().then((result) => setRelatedPosts(result));
+        }
+    }, [slug]);
+
+    return (
+        <div className="bg-white shadow-lg rounded-md p-8 mb-8">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+                {slug ? "Related Posts" : "Recent Posts"}
+            </h3>
+            {relatedPosts.map((post: any) => (
+                <div key={post.title} className="flex items-center w-full mb-4">
+                    <div className="w-16 flex-none">
+                        <img
+                            src={post.featuredImage.url}
+                            alt={post.title}
+                            width="60px"
+                            height="60px"
+                            className="align-middle rounded-full"
+                        />
+                    </div>
+                    <div className="flex-grow ml-4">
+                        <p className="text-gray-500 font-xs">
+                            {moment(post.createdAt).format("MMM DD, YYYY")}
+                        </p>
+                        <Link
+                            href={`/post/${post.slug}`}
+                            className="text-md"
+                            key={post.title}
+                        >
+                            {post.title}
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default PostWidget;
