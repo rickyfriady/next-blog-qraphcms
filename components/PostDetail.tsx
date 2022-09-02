846328261bb13a1acec2372e699cce92a35b9c60
AllukyyDev <73653891+rickyfriady@users.noreply.github.com>
2022-09-02 23:44:02 +0700

    Update PostDetail 1. make fetch data dan make gefragment 2. styling on postDetail components #. maping data on object

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -1,10 +1,75 @@
+import React from "react";
 import moment from "moment";
 
 interface PostDetailProps {
     post: any;
+    index: any;
+    text: any;
+    obj: any;
+    type: any;
+    typeObj: any;
+    item: any;
+    itemIndex: any;
 }
 
 const PostDetail = ({ post }: PostDetailProps) => {
+    const getContentFragment = (index, text, obj, type) => {
+        let modifiedText = text;
+
+        if (obj) {
+            if (obj.bold) {
+                modifiedText = <b key={index}>{text}</b>;
+            }
+
+            if (obj.italic) {
+                modifiedText = <em key={index}>{text}</em>;
+            }
+
+            if (obj.underline) {
+                modifiedText = <u key={index}>{text}</u>;
+            }
+        }
+
+        switch (type) {
+            case "heading-three":
+                return (
+                    <h3 key={index} className="text-xl font-semibold mb-4">
+                        {modifiedText.map((item, i) => (
+                            <React.Fragment key={i}>{item}</React.Fragment>
+                        ))}
+                    </h3>
+                );
+            case "paragraph":
+                return (
+                    <p key={index} className="mb-8">
+                        {modifiedText.map((item, i) => (
+                            <React.Fragment key={i}>{item}</React.Fragment>
+                        ))}
+                    </p>
+                );
+            case "heading-four":
+                return (
+                    <h4 key={index} className="text-md font-semibold mb-4">
+                        {modifiedText.map((item, i) => (
+                            <React.Fragment key={i}>{item}</React.Fragment>
+                        ))}
+                    </h4>
+                );
+            case "image":
+                return (
+                    <img
+                        key={index}
+                        alt={obj.title}
+                        height={obj.height}
+                        width={obj.width}
+                        src={obj.src}
+                    />
+                );
+            default:
+                return modifiedText;
+        }
+    };
+
     return (
         <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
             <div className="relative overflow-hidden shadow-md mb-6">
@@ -16,7 +81,7 @@ const PostDetail = ({ post }: PostDetailProps) => {
             </div>
             <div className="px-4 lg:px-0">
                 <div className="flex items-center mb-8 w-full">
-                    <div className="flex items-center mb-4 lg:mb-0 mr-8 lg:w-auto">
+                    <div className="flex items-center mb-4 lg:mb-0 mr-8 w-full lg:w-auto">
                         <img
                             src={post.author.photo.url}
                             alt={post.author.name}
@@ -43,11 +108,23 @@ const PostDetail = ({ post }: PostDetailProps) => {
                                 d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                             />
                         </svg>
-                        <span className="align-middle">
+                        <span>
                             {moment(post.createdAt).format("MMM DD, YYYY")}
                         </span>
                     </div>
                 </div>
+                <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+                {post.content.raw.children.map((typeObj, index) => {
+                    const children = typeObj.children.map((item, itemIndex) =>
+                        getContentFragment(itemIndex, item.text, item)
+                    );
+                    return getContentFragment(
+                        index,
+                        children,
+                        typeObj,
+                        typeObj.type
+                    );
+                })}
             </div>
         </div>
     );
