116946f20322092906438b8945b39c80cb2cd4bd
AllukyyDev <73653891+rickyfriady@users.noreply.github.com>
2022-09-04 17:38:46 +0700

    Update Commponent Author & CommmentsForm and [slug] page 1. change batter layout on author commponent 2. make layout and configure handelsubmisson and make validation on commentsForm commponets 3. slug page on progress :)

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,10 +1,25 @@
+import Image from "next/image";
+
 interface AuthorProps {
     author: any;
 }
 const Author = ({ author }: AuthorProps) => {
     return (
-        <div>
-            <p>Author</p>
+        <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+            <div className="absolute left-0 right-2 -top-14">
+                <Image
+                    src={author.photo.url}
+                    alt={author.name}
+                    unoptimized
+                    height="100px"
+                    width="100px"
+                    className="align-middle rounded-full"
+                />
+            </div>
+            <h3 className="text-white my-4 text-lg font-bold ">
+                {author.name}
+            </h3>
+            <p className="text-white text-lg">{author.bio}</p>
         </div>
     );
 };
--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -1,9 +1,96 @@
+import { useRef, useState, useEffect } from "react";
 interface CommentsFormProps {
     slug: any;
 }
 
 const CommentsForm = ({ slug }: CommentsFormProps) => {
-    return <div>CommentsForm</div>;
+    const [error, setError] = useState(false);
+    const [localStorage, setLocalStorage] = useState(null);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const CommentEl = useRef();
+    const nameEl = useRef();
+    const emailEl = useRef();
+    const storeDatatEl = useRef();
+
+    const handleCommentsSubmission = () => {
+        setError(false);
+
+        const { value: comment } = CommentEl.current;
+        const { value: name } = nameEl.current;
+        const { value: email } = emailEl.current;
+
+        if (!comment || !name || !email) {
+            setError(true);
+            return;
+        }
+        const commentObj = (name, email, comment, slug);
+    };
+
+    return (
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+                Comments
+            </h3>
+            <div className="grid  grid-cols-1 gap-4 mb-4">
+                <textarea
+                    ref={CommentEl}
+                    className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-300 bg-gray-200 text-gray-700"
+                    placeholder="type something for comment"
+                    name="comment"
+                ></textarea>
+            </div>
+            <div className="grid  grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+                <input
+                    type="text"
+                    ref={nameEl}
+                    className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-300 bg-gray-200 text-gray-700"
+                    placeholder="type your name"
+                    name="name"
+                />
+                <input
+                    type="text"
+                    ref={emailEl}
+                    className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-300 bg-gray-200 text-gray-700"
+                    placeholder="email"
+                    name="email"
+                />
+            </div>
+            <div className="grid  grid-cols-1 gap-4 mb-4">
+                <div>
+                    <input
+                        ref={storeDatatEl}
+                        type="checkbox"
+                        name="storeData"
+                        id="storeData"
+                        value="true"
+                    />
+                    <label
+                        className="text-gray-500 cursor-pointer ml-2"
+                        htmlFor="storeData"
+                    >
+                        Save my email and name for the next time i comments.
+                    </label>
+                </div>
+            </div>
+            {error && (
+                <p className="text-xs text-red-500">All Field is Reduired </p>
+            )}
+            <div className="mt-8">
+                <button
+                    type="button"
+                    onClick={handleCommentsSubmission}
+                    className="transition duration-500 ease hover:bg-indigo-500 inline-block bg-pink-600 rounded-full text-white px-8 py-3  cursor-pointer"
+                >
+                    Post Comment
+                </button>
+                {showSuccessMessage && (
+                    <span className="text-xl float-right font-semibold mt-3 text-green-500">
+                        Commment submitted for review
+                    </span>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default CommentsForm;
